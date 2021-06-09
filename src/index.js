952de073188a15115622fba70f7f77530358a77d
Geisson Machado <geisonnm@gmail.com>
2021-06-08 20:21:52 -0400

    Added React life cycle

--- src/index.js
+++ src/index.js
@@ -6,8 +6,26 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      message: "Mensagem Inicial!!"
+      message: "Mensagem Inicial!!",
+      counter: 0,
+      oldCounter: null
     }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.counter !== prevState.counter) {
+      this.setState({
+        oldCounter: prevState.counter
+      })
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        counter: this.state.counter + 1
+      });
+    }, 3000);
   }
 
   render() {
@@ -20,6 +38,10 @@ class App extends React.Component {
         }} />
 
         <h1>{this.state.message}</h1>
+
+        <h1>{this.state.counter}</h1>
+
+        <h1>Valor Antigo: {this.state.oldCounter}</h1>
 
         <h1>ISSO NÃO MUDA!</h1>
       </div>
